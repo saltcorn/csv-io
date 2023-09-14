54bcea68f4c6a5136e29daa37253e750fd333622
Tom Nielsen <tanielsen@gmail.com>
2023-09-14 08:45:25 +0100

    rename export to view

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "csv-io",
-  viewtemplates: [require("./export")],
+  viewtemplates: [require("./export_view")],
   actions: { import_csv_file: require("./import-file-action") },
 };
