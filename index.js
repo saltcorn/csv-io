8b1c0a614b632a912374284cb1ee7214fa9db441
Tom Nielsen <tanielsen@gmail.com>
2025-05-16 10:10:05 +0100

    json_to_csv function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { auto_expand_json_cols, async_stringify } = require("./common");
+
 module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "csv-io",
@@ -5,5 +7,26 @@ module.exports = {
   actions: {
     import_csv_file: require("./import-file-action"),
     export_csv_to_file: require("./export-file-action"),
+  },
+  functions: {
+    json_to_csv: {
+      async run(json_list) {
+        if (!json_list || !json_list.length) return "";
+        const columns = Object.keys(json_list[0]);
+        const str = await async_stringify(json_list, {
+          header: true,
+          columns,
+          delimiter: ",",
+          cast: {
+            date: (value) => value.toISOString(),
+            boolean: (v) => (v ? "true" : "false"),
+          },
+        });
+        return str;
+      },
+      description: "Convert a list of JSON objects to a CSV string",
+      isAsync: true,
+      arguments: [{ name: "json_list", type: "JSON" }],
+    },
   },
 };
