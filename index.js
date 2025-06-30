55e83eac506d242b5b39204666f11d8002d6955a
Tom Nielsen <tanielsen@gmail.com>
2025-06-30 14:52:55 +0100

    testing; 0.4.1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   sc_plugin_api_version: 1,
   plugin_name: "csv-io",
   viewtemplates: [require("./export_view")],
+  testString: "testing",
   actions: {
     import_csv_file: require("./import-file-action"),
     export_csv_to_file: require("./export-file-action"),
